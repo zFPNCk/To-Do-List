26c2eb82bacbe7071d3fecb7294a1d032e17d024
Nicolas Guimarães <nicolasguima_fpnc@hotmail.com>
2024-11-19 17:35:17 -0300

    add web.py

---
+++ web.py
@@ -0,0 +1,24 @@
+# Start the Server with the command: "python -m streamlit run web.py"
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.appends(todo)
+    functions.write_todos(todos)
+
+st.title("My Todo App")
+st.write("This app is to increase your productivity.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
